57cd9c64dc6660cf1001f3261d3068e7daaf336a
Erlich Sefi <erlichsefi@gmail.com>
2024-09-15 22:08:03 +0300

    Create daliy_raw_dump.py

---
+++ daliy_raw_dump.py
@@ -0,0 +1,14 @@
+from il_supermarket_scarper import MainScrapperRunner
+from  il_supermarket_parsers import ParallelParser
+
+
+number_of_processes = 6
+data_folder = "dumps"
+
+scraper = MainScrapperRunner()
+scraper.run()
+
+ParallelParser(
+      data_folder,
+      number_of_processes=number_of_processes,
+).execute()
