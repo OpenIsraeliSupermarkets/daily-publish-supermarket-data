caf9ff10c5156c8d092e09d633cc2d49190fd67a
Sefi Erlich <erlichsefi@gmail.com>
2024-09-22 00:36:58 +0300

    v0

--- daliy_raw_dump.py
+++ daliy_raw_dump.py
@@ -3,22 +3,35 @@ from il_supermarket_parsers import ConvertingTask
 from kaggle import KaggleDatasetManager
 
 
-number_of_processes = 6
-data_folder = "dumps"
-enabled_scrapers=[ScraperFactory.BAREKET.name]
+if __name__ == "__main__":
+    number_of_processes = 6
+    data_folder = "dumps"
+    outputs_folder = "outputs"
+    enabled_scrapers=[ScraperFactory.BAREKET.name]
 
-scraper = ScarpingTask(
-    enabled_scrapers=enabled_scrapers,
-    multiprocessing=number_of_processes,
-    dump_folder_name=data_folder
-)
-scraper.start()
-
-converter = ConvertingTask(
-      data_folder=data_folder,
-      number_of_processes=number_of_processes,
-)
-files = converter.start()
+    ScarpingTask(
+        enabled_scrapers=enabled_scrapers, #download one from each 
+        dump_folder_name=data_folder,
+        limit=1,
+        multiprocessing=number_of_processes,
+        lookup_in_db=True
+    ).start()
+    scraper = ConvertingTask(
+        enabled_parsers=enabled_scrapers,
+        data_folder=data_folder,
+        multiprocessing=number_of_processes,
+        output_folder=outputs_folder
+    ).start()
 
 database =  KaggleDatasetManager()
 database.upload_to_dataset("israeli-supermarkets-2024", data_folder)
+
+
+
+from il_supermarket_parsers import ConvertingTask
+from il_supermarket_scarper import ScarpingTask,ScraperFactory
+if __name__ == "__main__":
+
+
+    multiprocessing = None
+
