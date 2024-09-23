5c20bbaabdaa879bd9a640227c7baeed7e55ec9e
Sefi Erlich <erlichsefi@gmail.com>
2024-09-23 18:04:59 +0300

    .

--- daliy_raw_dump.py
+++ daliy_raw_dump.py
@@ -1,9 +1,8 @@
-from il_supermarket_scarper import ScarpingTask,ScraperFactory
+from il_supermarket_scarper import ScarpingTask
 from il_supermarket_parsers import ConvertingTask
 from kaggle_database_manager import KaggleDatasetManager
-import os
 import shutil
-import json
+import datetime
 
 
 if __name__ == "__main__":
@@ -11,36 +10,24 @@ if __name__ == "__main__":
     data_folder = "dumps"
     outputs_folder = "outputs"
     status_folder = "status"
-    enabled_scrapers=None
+    enabled_scrapers = None
 
     ScarpingTask(
-        enabled_scrapers=enabled_scrapers, #download one from each 
+        enabled_scrapers=enabled_scrapers,  # download one from each
         dump_folder_name=data_folder,
         multiprocessing=number_of_processes,
         lookup_in_db=True,
-        only_latest=True
+        only_latest=True,
     ).start()
     scraper = ConvertingTask(
         enabled_parsers=enabled_scrapers,
         data_folder=data_folder,
         multiprocessing=number_of_processes,
-        output_folder=outputs_folder
+        output_folder=outputs_folder,
     ).start()
 
-    shutil.rmtree("israeli-supermarkets-2024",ignore_errors=True)
-    os.makedirs("israeli-supermarkets-2024",exist_ok=True)
-    with open("israeli-supermarkets-2024/dataset-metadata.json",'w') as file:
-        json.dump({
-        "title": "Israeli Supermarkets 2024", 
-        "id": "erlichsefi/israeli-supermarkets-2024", 
-        "licenses": [{"name": "CC0-1.0"}]
-        },file)
-    shutil.copytree(outputs_folder, "israeli-supermarkets-2024",dirs_exist_ok=True)
-    shutil.rmtree(outputs_folder)
-    shutil.copytree(status_folder, "israeli-supermarkets-2024",dirs_exist_ok=True)
-    shutil.rmtree(status_folder)
-    shutil.rmtree(data_folder)
-
     database = KaggleDatasetManager(dataset="israeli-supermarkets-2024")
+    database.compose(outputs_folder=outputs_folder, status_folder=status_folder)
     database.upload_to_dataset(version_notes="first try")
 
+    shutil.rmtree(data_folder)
--- kaggle_database_manager.py
+++ kaggle_database_manager.py
@@ -1,26 +1,60 @@
 import os
+import shutil
+import json
+import datetime
+import pytz
+
 
 class KaggleDatasetManager:
     def __init__(self, dataset):
         from kaggle import KaggleApi
+
         self.api = KaggleApi()
         self.api.authenticate()
         self.dataset = dataset
-        # self.status_files = status_files
 
-    # def download_status_files(self):
-    #     """
-    #     Download a dataset from Kaggle.
+    def _now():
+        return datetime.datetime.now(pytz.timezone("Asia/Jerusalem")).strftime(
+            "%d/%m/%Y, %H:%M:%S"
+        )
 
-    #     :param dataset: str, the dataset to download in the format 'owner/dataset-name'
-    #     :param path: str, the path where to save the dataset (default is current directory)
-    #     """
-    #     for status_file in self.status_files:
-    #         try:
-    #             self.api.dataset_download_cli(self.dataset, file_name=status_file)
-    #             print(f"Dataset '{self.dataset}' downloaded successfully")
-    #         except Exception as e:
-    #             print(f"Error downloading dataset: {e}")
+    def compose(self, outputs_folder, status_folder):
+        shutil.rmtree(self.dataset, ignore_errors=True)
+        os.makedirs(self.dataset, exist_ok=True)
+        with open(f"{self.dataset}/dataset-metadata.json", "w") as file:
+            json.dump(
+                {
+                    "title": "Israeli Supermarkets 2024",
+                    "id": f"erlichsefi/{self.dataset}",
+                    "licenses": [{"name": "CC0-1.0"}],
+                },
+                file,
+            )
+        shutil.copytree(outputs_folder, self.dataset, dirs_exist_ok=True)
+        shutil.rmtree(outputs_folder)
+        shutil.copytree(status_folder, self.dataset, dirs_exist_ok=True)
+        shutil.rmtree(status_folder)
+
+        self.increase_index()
+
+    def increase_index(self):
+        """
+        Download a dataset from Kaggle.
+
+        :param dataset: str, the dataset to download in the format 'owner/dataset-name'
+        :param path: str, the path where to save the dataset (default is current directory)
+        """
+
+        self.api.dataset_download_cli(self.dataset, file_name="index.json")
+        print(f"Dataset '{self.dataset}' downloaded successfully")
+
+        with open("index.json", "r") as file:
+            index = json.loads(file)
+
+        index[max(map(int, index.keys())) + 1] = self._now()
+
+        with open(os.path.join(self.dataset, "index.json"), "w") as file:
+            json.dump(index, file)
 
     def upload_to_dataset(self, version_notes=None):
         """
@@ -31,13 +65,16 @@ class KaggleDatasetManager:
         :param new_file_name: str, optional new name for the file in the dataset
         """
         try:
-            self.api.dataset_create_version(self.dataset, version_notes=version_notes,delete_old_versions=False) # each day is a version
+            self.api.dataset_create_version(
+                self.dataset, version_notes=version_notes, delete_old_versions=False
+            )  # each day is a version
         except Exception as e:
             print(f"Error uploading file: {e}")
+
 
 # Example usage:
 if __name__ == "__main__":
     manager = KaggleDatasetManager("israeli-supermarkets-2024")
 
     # Upload a folder to a dataset (make sure you have write permissions)
-    manager.upload_to_dataset(version_notes='now')
+    manager.upload_to_dataset(version_notes="now")
