fb60815dcaeea0b23ad0a1fdfee00adff14655b0
Sefi Erlich <erlichsefi@gmail.com>
2024-10-16 11:42:11 +0000

    remove to a batching approch

--- daliy_raw_dump.py
+++ daliy_raw_dump.py
@@ -1,27 +1,49 @@
 import shutil
+import schedule
+import time
+
 import os
 from il_supermarket_scarper import ScarpingTask
 from il_supermarket_parsers import ConvertingTask
 from kaggle_database_manager import KaggleDatasetManager
 
+number_of_processes = 4
+data_folder = "app_data/dumps"
+outputs_folder = "app_data/outputs"
+status_folder = "app_data/dumps/status"
+enabled_scrapers = None
+enabled_file_types = None
+occasions = ["12:00", "17:00", "23:00"]
+executed_jobs = 0
 
-if __name__ == "__main__":
-    number_of_processes = 4
-    data_folder = "app_data/dumps"
-    outputs_folder = "app_data/outputs"
-    status_folder = "app_data/dumps/status"
-    enabled_scrapers = None
-    enabled_file_types = None
 
+def run_scraping():
     try:
         ScarpingTask(
-            enabled_scrapers=enabled_scrapers,  
+            enabled_scrapers=enabled_scrapers,
             files_types=enabled_file_types,
             dump_folder_name=data_folder,
             multiprocessing=number_of_processes,
             lookup_in_db=True,
             only_latest=True,
         ).start()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    #
+    for occasion in occasions:
+        schedule.every().day.at(occasion).do(run_scraping)
+    
+    #   
+    try:
+        # 
+        while executed_jobs < len(occasions):
+            schedule.run_pending()
+            time.sleep(1)
+        
+        #
         ConvertingTask(
             enabled_parsers=enabled_scrapers,
             files_types=enabled_file_types,
@@ -30,14 +52,18 @@ if __name__ == "__main__":
             output_folder=outputs_folder,
         ).start()
 
-        database = KaggleDatasetManager(dataset="israeli-supermarkets-2024",enabled_scrapers=enabled_scrapers,enabled_file_types=enabled_file_types)
+        database = KaggleDatasetManager(
+            dataset="israeli-supermarkets-2024",
+            enabled_scrapers=enabled_scrapers,
+            enabled_file_types=enabled_file_types,
+        )
         database.compose(outputs_folder=outputs_folder, status_folder=status_folder)
         database.upload_to_dataset()
         database.clean()
-    
-    finally:  
+
+    finally:
         # clean the folders in case of an error
-        for folder in [data_folder,outputs_folder,status_folder]:
-            
+        for folder in [data_folder, outputs_folder, status_folder]:
+
             if os.path.exists(folder):
                 shutil.rmtree(folder)
