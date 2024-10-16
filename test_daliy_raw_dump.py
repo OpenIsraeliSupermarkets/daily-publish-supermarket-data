00dfc1bf2e09bf1d5c0adc465dc4d578366b55ef
Sefi Erlich <erlichsefi@gmail.com>
2024-10-16 19:01:58 +0300

    .

--- test_daliy_raw_dump.py
+++ test_daliy_raw_dump.py
@@ -10,17 +10,19 @@ from il_supermarket_scarper.scrappers_factory import ScraperFactory
 class TestSupermarketDataPublisher(unittest.TestCase):
         
 
-    @patch("daliy_raw_dump.KaggleDatasetManager")
+    @patch("daliy_raw_dump.KaggleDatasetManager.clean")
+    @patch("daliy_raw_dump.KaggleDatasetManager.upload_to_dataset")
+    @patch("daliy_raw_dump.KaggleDatasetManager.increase_index")
     def test_upload_to_kaggle_with_limited_scrapers_and_files(
-        self, MockKaggleDatasetManager
+        self, mock_upload_to_dataset, mock_clean_folders, mock_increase_index
     ):
-        # Arrange
+        # arrange
         num_of_occasions = 3
         
         self.publisher = SupermarketDataPublisher(
-            enabled_scrapers = ScraperFactory.sample(1),
-            limit = 1,
-            completed_by = datetime.datetime.now() + datetime.timedelta(minutes=num_of_occasions),
+            enabled_scrapers=ScraperFactory.sample(1),
+            limit=1,
+            completed_by=datetime.datetime.now() + datetime.timedelta(minutes=num_of_occasions),
             num_of_occasions=num_of_occasions
         )
         self.publisher._clean_folders()
@@ -28,13 +30,11 @@ class TestSupermarketDataPublisher(unittest.TestCase):
         self.publisher.run()
 
         # Assert
-        mock_manager = MockKaggleDatasetManager.return_value
-        mock_manager.compose.assert_called_once_with(
-            outputs_folder=self.publisher.outputs_folder,
-            status_folder=self.publisher.status_folder,
-        )
-        mock_manager.upload_to_dataset.assert_called_once()
-        mock_manager.clean.assert_called_once()
+        mock_upload_to_dataset.assert_called_once()
+        mock_clean_folders.assert_called_once()
+        mock_increase_index.assert_called_once()
+        
+        self.publisher._clean_folders()
 
 
 if __name__ == "__main__":
