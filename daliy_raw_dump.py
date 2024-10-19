032ff43916c75f62fd8265815fe029a4c0eab7bb
Sefi Erlich <erlichsefi@gmail.com>
2024-10-19 13:46:35 +0300

    .

--- daliy_raw_dump.py
+++ daliy_raw_dump.py
@@ -21,7 +21,7 @@ class BaseSupermarketDataPublisher:
 
     def __init__(
         self,
-        number_of_processes=4,
+        number_of_processes=3,
         app_folder="app_data",
         data_folder="dumps",
         outputs_folder="outputs",
