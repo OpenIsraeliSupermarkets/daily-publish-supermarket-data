09c991661b8f7ad47f0c75075ed4b75a4e9367dd
Sefi Erlich <erlichsefi@gmail.com>
2025-04-21 17:00:44 +0300

    fox

--- act.sh
+++ act.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+set -e
+
 if [ -f .env.test ]; then
     export $(cat .env.test | xargs)
 fi
 
 echo "Running production API deployment workflow..."
-act -W '.github/workflows/_prod_deploy_api.yml' -P self-hosted=catthehacker/ubuntu:act-latest
+act -W '.github/workflows/_stage_deploy_api.yml' -P self-hosted=catthehacker/ubuntu:act-latest || { echo "API deployment workflow failed. Aborting."; exit 1; }
 echo "Production API deployment workflow completed"
 
 echo "Running production scraping workflow..."
-act -W '.github/workflows/_prod_scrape.yml' -P self-hosted=catthehacker/ubuntu:act-latest
+act -W '.github/workflows/_stage_scrape.yml' -P self-hosted=catthehacker/ubuntu:act-latest || { echo "Scraping workflow failed. Aborting."; exit 1; }
 echo "Production scraping workflow completed"
 
 echo "Running production publishing workflow..."
-act -W '.github/workflows/_prod_publishing.yml' -P self-hosted=catthehacker/ubuntu:act-latest
+act -W '.github/workflows/_stage_publishing.yml' -P self-hosted=catthehacker/ubuntu:act-latest || { echo "Publishing workflow failed. Aborting."; exit 1; }
 echo "Production publishing workflow completed"
