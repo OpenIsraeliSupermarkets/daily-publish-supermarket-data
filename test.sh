c2f4d0b9df9000acdf60a8650abb404e51b4a8f6
Sefi Erlich <erlichsefi@gmail.com>
2025-04-25 10:17:49 +0300

    clean

--- test.sh
+++ test.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 export $(cat .secrets | xargs)
 
+# Clean the app data folder
+if [ -d "$APP_DATA_PATH" ]; then
+    rm -rf "$APP_DATA_PATH"/*
+    echo "Cleaned contents of $APP_DATA_PATH"
+else
+    echo "Warning: $APP_DATA_PATH directory does not exist"
+fi
+
+# Clean the mongo data folder
+if [ -d "$MONGO_DATA_PATH" ]; then
+    rm -rf "$MONGO_DATA_PATH"/mongo_data/*
+    echo "Cleaned contents of $MONGO_DATA_PATH"
+else
+    echo "Warning: $MONGO_DATA_PATH directory does not exist"
+fi
+
+
 docker compose build --no-cache
 docker compose up 
 
