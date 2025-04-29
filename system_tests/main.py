065cbf2ea076a7b9a7584c801fa89d05f1be99ec
Sefi Erlich <erlichsefi@gmail.com>
2025-04-29 22:19:09 +0300

    system test

--- system_tests/main.py
+++ system_tests/main.py
@@ -30,31 +30,31 @@ async def run_validations():
         )
     )
 
-    # # Data serving validation task
-    # tasks.append(
-    #     asyncio.create_task(
-    #         full_data_scan(
-    #             os.getenv("API_TOKEN"),
-    #             os.getenv("API_HOST"),
-    #             int(os.getenv("RATE_LIMIT", "3")),
-    #         )
-    #     )
-    # )
+    # Data serving validation task
+    tasks.append(
+        asyncio.create_task(
+            full_data_scan(
+                os.getenv("API_TOKEN"),
+                os.getenv("API_HOST"),
+                int(os.getenv("RATE_LIMIT", "3")),
+            )
+        )
+    )
 
     # Data kaggle validation task
-    # tasks.append(
-    #     asyncio.create_task(
-    #         asyncio.to_thread(
-    #             validate_data_storage,
-    #             os.getenv("KAGGLE_DATASET_REMOTE_NAME"),
-    #             os.getenv("ENABLED_SCRAPERS",",".join(ScraperFactory.all_scrapers_name())).split(","),
-    #             os.getenv("MONGODB_URI"),
-    #             file_per_run=int(os.getenv("LIMIT")) if os.getenv("LIMIT") else None,
-    #             num_of_occasions=int(os.getenv("NUM_OF_OCCASIONS")) if os.getenv("NUM_OF_OCCASIONS") else None,
-    #             upload_to_long_term_db=True # assume running after the publish
-    #         )
-    #     )
-    # )
+    tasks.append(
+        asyncio.create_task(
+            asyncio.to_thread(
+                validate_data_storage,
+                os.getenv("KAGGLE_DATASET_REMOTE_NAME"),
+                os.getenv("ENABLED_SCRAPERS",",".join(ScraperFactory.all_scrapers_name())).split(","),
+                os.getenv("MONGODB_URI"),
+                file_per_run=int(os.getenv("LIMIT")) if os.getenv("LIMIT") else None,
+                num_of_occasions=int(os.getenv("NUM_OF_OCCASIONS")) if os.getenv("NUM_OF_OCCASIONS") else None,
+                upload_to_long_term_db=True # assume running after the publish
+            )
+        )
+    )
 
     # Wait for both tasks to complete
     await asyncio.gather(*tasks)
