0b0e51ea611cd2d7b9bdf2e663056a1c6ded6b88
Sefi Erlich <erlichsefi@gmail.com>
2025-04-29 21:19:22 +0300

    high data lost

--- system_tests/data_processing_validation.py
+++ system_tests/data_processing_validation.py
@@ -169,10 +169,7 @@ def validate_data_processing(uri="mongodb://192.168.1.129:27017/"):
     # קבלת חותמות זמן לכל קובץ
     file_name_dict = get_file_timestamps(scraper_status_collection)
     validation_results = dict()
-    aggregated_errors = {
-        "downloaded": defaultdict(int),
-        "parsed": defaultdict(int),
-    }
+    aggregated_errors = {}
 
     for chain in DumpFolderNames:
 
@@ -185,7 +182,13 @@ def validate_data_processing(uri="mongodb://192.168.1.129:27017/"):
 
         validation_results[chain_name] = {}
         used_timestamp = []
-        saw = []
+        chain_errors = {
+            "downloaded": [],
+            "parsed": [],
+            "overall_file_saw": [],
+            "overall_lost_files": [],
+            "overall_download_files": [],
+        }
         for itreation_timestamp in file_name_dict[folder_name.lower()]:
 
             validation_results[chain_name][itreation_timestamp] = {}
@@ -215,7 +218,7 @@ def validate_data_processing(uri="mongodb://192.168.1.129:27017/"):
 
             # בדיקת סטטוס עבור כל קובץ
             pipeline = {
-                "new_files": list(set(files_saw) - set(saw)),
+                "new_files": list(set(files_saw) - set(chain_errors["overall_file_saw"])),
                 "saw": len(files_saw),
                 "fail_downloaded": 0,
                 "not_collected_by_parser": 0,
@@ -226,31 +229,37 @@ def validate_data_processing(uri="mongodb://192.168.1.129:27017/"):
                 # failed at download
                 if file in downloaded_files_failed:
                     pipeline["fail_downloaded"] += 1
-                    aggregated_errors["downloaded"][downloaded_files_failed[file]] += 1
+                    chain_errors["downloaded"].append(downloaded_files_failed[file])
+                    chain_errors["overall_lost_files"].append(file)
                 # not failed
                 elif file in downloaded_files_success:
 
                     if file not in files_to_parse:
                         pipeline["not_collected_by_parser"] += 1
                     elif file in parsing_results_failed:
-                        aggregated_errors["parsed"][parsing_results_failed[file]] += 1
                         pipeline["fail_parsed"] += 1
+                        chain_errors["parsed"].append(parsing_results_failed[file])
+                        chain_errors["overall_lost_files"].append(file)
+                        
                     elif file in parsing_results_success:
                         pipeline["succesful_processed"] += 1
+                        chain_errors["overall_download_files"].append(file)
                     else:
                         raise ValueError(f"file {file} is not in any of the lists")
                 else:
                     raise ValueError(f"file {file} is not in any of the lists")
-            saw = set(files_saw) | set(saw)
+            chain_errors["overall_file_saw"] = list(set(files_saw) | set(chain_errors["overall_file_saw"]))
             validation_results[chain_name][itreation_timestamp] = pipeline
+        
+        aggregated_errors[chain_name] = chain_errors
 
     with open("validation_results.json", "w") as f:
         json.dump(validation_results, f, indent=4)
     with open("aggregated_errors.json", "w") as f:
         json.dump(aggregated_errors, f, indent=4)
 
-    if not all(list(map(lambda x: x == {}, aggregated_errors.values()))):
-        raise ValueError("found errors in the data")
+    for chain, errors in aggregated_errors.items():
+        assert len(set(errors["overall_lost_files"]) - set(errors['overall_download_files'])) / len(errors['overall_file_saw']) < 0.02, f"chain {chain} data lost in the data processing pipeline is too high"
 
 
 if __name__ == "__main__":
